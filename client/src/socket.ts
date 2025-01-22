d9927948de1aefcf65ac700b0643925999d4bb67
amanmakhija <amanmakhija2205@gmail.com>
2025-01-22 19:35:49 +0530

    updating the env valriables

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL =
-  import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_SERVER_BASE_URL
-    : import.meta.env.VITE_SERVER_BASE_URL_DEV;
+const URL = import.meta.env.VITE_SERVER_BASE_URL;
 
 export const socket = io(URL, { autoConnect: false });
